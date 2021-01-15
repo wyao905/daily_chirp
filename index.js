41f5570022c19e73317ef37e6f80848668315dce
wyao905 <warrenyca905@gmail.com>
2021-01-15 15:46:55 -0500

    implemented twitter npm package and added search for tweets by geocode

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
+const Twitter = require('twitter')
 const express = require('express')
+require('dotenv/config')
+
+const apiKey = process.env.apiKey
+const apiKeySecret = process.env.apiKeySecret
+const accessToken = process.env.accessToken
+const accessTokenSecret = process.env.accessTokenSecret
+const bearerToken = process.env.bearerToken
+
 const app = express()
+const client = new Twitter({
+    consumer_key: apiKey,
+    consumer_secret: apiKeySecret,
+    access_token_key: accessToken,
+    access_token_secret: accessTokenSecret
+    // bearer_token: bearerToken
+})
 
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    client.get('search/tweets', { geocode: '34.0522,118.2437,10mi' }, (error, tweets) => {
+        if (error) throw error
+        res.send(tweets)
+    })
 })
 
-app.get('/tweets', (req, res) => {
-    res.send('tweets')
-})
+// app.get('/tweets', (req, res) => {
+//     res.send('tweets')
+// })
 
 // parameters
 // app.get('/tweets/:id', (req, res) => {
@@ -24,6 +43,8 @@ app.get('/tweets', (req, res) => {
 //     }
 //     res.send(course)
 // })
+
+
 
 const port = process.env.PORT || 3000
 app.listen(port)
